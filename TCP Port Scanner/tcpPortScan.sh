743ec8bcc0d9a64c377654b25e0676c1679eea37
nobodyatall648 <35725871+nobodyatall648@users.noreply.github.com>
2022-01-20 16:49:25 +0800

    update bash version TCP port scanner

--- TCP Port Scanner/tcpPortScan.sh
+++ TCP Port Scanner/tcpPortScan.sh
@@ -4,6 +4,7 @@
 
 #global var
 host="$1"
+counter=0
 
 # this function is called when Ctrl-C is sent
 function trap_ctrlc ()
@@ -15,22 +16,22 @@ function trap_ctrlc ()
  
     echo "[*] Exiting TCP Port Scanner..."
  
-    # exit shell script with error code 2
-    # if omitted, shell script will continue execution
     exit 2
 }
  
-# initialise trap to call trap_ctrlc function
-# when signal 2 (SIGINT) is received
 trap "trap_ctrlc" 2
 
 #perform TCP port scanning from 1-65535 port ranges
 for port in {1..65535}; do
 	if timeout 5 bash -c "(</dev/tcp/$host/$port) &>/dev/null " ; then
 		echo "[*] $port Port Open."
-
+		((counter++))
 	# un-comment this block of code: include close port statement printing 
 	# else
 	# 	echo "[!] $port Port Closed."
 	fi
 done
+
+echo "================================="
+echo "[*] Completed All Ports Scanning."
+echo "Port Detected Open: $counter"
